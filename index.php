b6032e3ed9d82bb082b1a2770622a75f9480b7c4
Joshua <joshuavtk@hotmail.com>
2017-09-30 23:34:07 +0200

    sass files added
    and added some content in index.php

---
+++ index.php
@@ -0,0 +1,29 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+    <link href="http://fonts.googleapis.com/css?family=Lato:300,400,700" rel="stylesheet" type="text/css">
+    <link rel="stylesheet" href="sass/main.css" type="text/css">
+    <title>Portfolio | Joshua van 't Kruis</title>
+</head>
+<body>
+<header>
+    <h1 class="header__title title--size-big">Joshua van 't Kruis</h1>
+    <h3 class="header__title title--size-medium">Media developer</h3>
+</header>
+<main>
+
+    <h2>MaanRoosVis</h2>
+    <h3>Project The Wall</h3>
+    <a href="https://github.com/RickHinfelaar/TheWallMaanRoosVis">Github link</a>
+
+</main>
+<footer>
+
+</footer>
+</body>
+</html>
